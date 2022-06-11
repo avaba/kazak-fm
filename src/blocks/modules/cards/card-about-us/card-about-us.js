ec4b2ce7ea87df288c26303ec88a8ae8a4d4c0e1
avaba <42552832+avaba@users.noreply.github.com>
2022-06-11 04:44:11 +0300

    fix

--- src/blocks/modules/cards/card-about-us/card-about-us.js
+++ src/blocks/modules/cards/card-about-us/card-about-us.js
@@ -1,7 +1,13 @@
 import $ from "jquery";
 
-$('.card-about').on('click', function () {
-    let content = $(this).html()
-    $('.popup__about .popup__text').html(content);
-    $('.popup__about').addClass('active')
-})
+$(".card-about").on("click", function () {
+    let content = $(this).html();
+    $(".popup__about .popup__text").html(content);
+    $(".popup__about").addClass("active");
+});
+
+$(".author__avatar--img").on("click", function () {
+    let content = $(this).find(".author-info").html();
+    $(".popup__about .popup__text").html(content);
+    $(".popup__about").addClass("active");
+});
